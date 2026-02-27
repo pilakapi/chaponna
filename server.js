9c69e1d7b56b43b9084ec49f83810afa3d91c558
pilakapi <jonathansalin22@gmail.com>
2026-02-27 13:12:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const Database = require('better-sqlite3');
+
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const https = require('https');
